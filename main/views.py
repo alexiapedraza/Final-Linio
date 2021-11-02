3fe63f8b2ec2ee365884f4330e30e495b40804a9
alexiapedraza <a.pedrazai@alum.up.edu.pe>
2021-11-02 18:11:46 -0500

    final

--- main/views.py
+++ main/views.py
@@ -24,7 +24,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_products'] = Producto.objects.all()[:5]
+        context['latest_products'] = Producto.objects.all()[:4]
 
         return context
 
